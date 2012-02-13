2424afb6b604384e95972c2b671fad9583f85806
Joel Martin <jmartin@lonocloud.com>
2012-02-13 13:01:32 -0600

    Use single function. Working on Firefox 9.
    Uses one big function conditionalized on the arguments to save on
    "function" and "return".
    
    This gives space for firefox fixes and using pageX,pageY so that mouse
    is relative to the whole document not just the visible area.
    
    No logging, comments, leading spaces: 1143 bytes
    After packer 3.0: 1012 bytes

--- vnc.js
+++ vnc.js
@@ -1,43 +1,83 @@
-//for(i in a){a[i[0]+i[7]]=a[i]}
+// TODO:
+// - mouse drag
+// - mouse middle,right,wheel (suppress default behavior)
+// - special key 0xff00 translation
+// - cross-browser
+// - close alert
 b.style.margin=r=s=v=0;
 Q=u=[];
 O=255;
-S = new WebSocket("ws://"+location.search.slice(1),"base64");
-// S.send, peek short, or push short depending on arguments
-P=function(a,b,c) {
-    //console.log("P: " + a.slice(0,5) + "... (" + b + "," + c + ")");
-    //if (!(c+1) && !(b+1)) console.log("Sending: " + a);
-    return (c+1 ? a.concat(b>>8,b&O,c>>8,c&O) : b+1 ? (a[b]<<8)+a[++b] : S.send(btoa(a.pop ? a.map(function(x) {return String.fromCharCode(x)}).join("") : a)))
-};
-U=function(d,e) {P(d+1 ? [4,d,0,0,0,0,0,e.which] : [3,v].concat(u));v=1}
+W=MozWebSocket||WebSocket;
+S = new W("ws://"+location.search.slice(1),"base64");
+// F(event.type==ke[y]press) -> onkeypress
+// F(event.type==ke[y]press,down) -> send key event
+// F(event.type==mo[u]sedown) -> send mousedown event
+// F(event.type==mo[u]seup) -> send mouseup event
+// F(event.type==mo[u]semove) -> send mouse move event
+// F(array,b,c) -> push shorts b and c
+// F(array,b) -> peek short array[b]<<8+array[b+1]
+// F(array) -> send array via websocket
+// F(string,-1) -> send string via websocket
+// F(character) -> return charCodeAt(0)
+// F(number) -> return fromCharCode(number)
+// F() -> send FBU request
+F=function(a,b,c) {
+    a=a?a:NaN;
+    t=(T=a.type)?T[2]:0;
+//    t=='y'?
+//                b+1? console.log("F: " + "send keypress") : console.log("F: " + "onkeypress")
+//            :t=='u'? console.log("F: " + "onmouse*")
+//            //:t=='s'? "onmessage"
+//            :a.pop?
+//                c+1? console.log("F: " + "push shorts")
+//                :b+1? console.log("F: " + "peek short")
+//                //: F(a.map(function(x) {return String.fromCharCode(x)}).join(""),-1)
+//                : console.log("F: " + "send array")
+//            :b<0? console.log("F: " + "send string")
+//            :a[0]? null
+//            :b+1? null
+//            : console.log("F: " + "send FBU request");
+    return t=='y'?
+                b+1? F([4,b,0,0,0,0,0,a.which]) : (F(a,1),F(a,0))
+            :t=='u'? F(F([5,T[5]=="d"?1:0],a.pageX,a.pageY))
+            :a.pop?
+                c+1? a.concat(b>>8,b&O,c>>8,c&O)
+                :b+1? (a[b]<<8)+a[b+1] 
+                //: F(a.map(function(x) {return String.fromCharCode(x)}).join(""),-1)
+                : F(a.map(F).join(""),-1)
+            :b<0? S.send(btoa(a))
+            :a[0]? a.charCodeAt(0)
+            :b+1? String.fromCharCode(a)
+            :(F([3,v].concat(u)),v=1)
+}
 S.onmessage = function (e) {
-    q = [].map.call(atob(e.data),function(x){return x.charCodeAt(0)});
+    q = [].map.call(atob(e.data),F);
     //if (q.length < 30) { console.log("q (" + q.length + "): " + q);
     //} else { console.log("q[0..30] (" + q.length + "): " + q.slice(0,30)) }
     if (s==2) {
-        w = c.width = P(q,0);
-        h = c.height = P(q,2);
+        w = c.width = F(q,0);
+        h = c.height = F(q,2);
         console.log("Connected: width " + w + " height " + h);
-        u = P([0,0,0,0],w,h);
-        setInterval(U,O);
-            //setTimeout(function(){U();}, 200);
-        onmousedown=onmouseup=onmousemove=onkeypress=function(e) {e.type=="keypress" ? (U(1,e),U(0,e)) : P(P([5,e.type=="mousedown"?1:0],e.x,e.y))}
+        u = F([0,0,0,0],w,h);
+        setInterval(F,O);
+            //setTimeout(F, 200);
+        onmousedown=onmouseup=onmousemove=onkeypress=F;
     }
     if (s<3) {
-        P(["RFB 003.003\n",[1],[2,0,0,1,0,0,0,0]][s++])
+        F(["RFB 003.003\n",[1],[2,0,0,1,0,0,0,0]][s++],-1)
     } else {
         L = Q.push.apply(Q,q);
         t = Q[0];
         if (!r) {
-            l = [20,1,2,8+P(Q,6)][t];
+            l = [20,1,2,8+F(Q,6)][t];
             if (L<l) return;
-            if (t==0) r=P(Q,2),l=4;
+            if (t==0) r=F(Q,2),l=4;
             //if (t==0) console.log("new FBU, rects: " + r);
             Q.splice(0,l)
         }
         for(;r;r--) {
             // Process it
-            w=P(Q,4),h=P(Q,6);
+            w=F(Q,4),h=F(Q,6);
             l = 12+4*w*h;
             //console.log("Q.length: " + Q.length + " size: 12 + 4*" + w + "x" + h + "=" + (12 + 4*w*h));
             if (!l||Q.length<l) break;
@@ -49,8 +89,9 @@ S.onmessage = function (e) {
                 for (k=0;k<3;k++) z[i+k]=Q[j+2-k];
                 z[i+3] = O; // Set Alpha
             }
-            a.putImageData(I,P(Q,0),P(Q,2));
+            a.putImageData(I,F(Q,0),F(Q,2));
             Q.splice(0,l)
         }
     }
 }
+S.onclose = function (e) { console.log("closed: " + e.code + ", " + e.reason); }
